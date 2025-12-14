4f8da873c89228d44d26ffb644de2a491eae6eed
syahruromadona <amierulaimanomar@gmail.com>
2025-12-14 12:36:25 +0800

    new

--- server/db.js
+++ server/db.js
@@ -2,13 +2,10 @@
    const dotenv = require('dotenv') 
     dotenv.config();
 
-    const pool = new Pool ({
-       connectionString: process.env.DATABASE_URL,
-        ssl:
-         process.env.NODE_ENV === "production"
-            ? { rejectUnauthorized: false }
-            : false,
-    })
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
+});
 
 
     module.exports = pool;
