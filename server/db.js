faee8a696e435072129f752a7587d84e35a940aa
syahruromadona <amierulaimanomar@gmail.com>
2025-12-14 12:33:12 +0800

    new

--- server/db.js
+++ server/db.js
@@ -4,7 +4,10 @@
 
     const pool = new Pool ({
        connectionString: process.env.DATABASE_URL,
-       ssl: process.env.NODE_ENV === "production" ? {rejectUnauthorized: false} : false
+        ssl:
+         process.env.NODE_ENV === "production"
+            ? { rejectUnauthorized: false }
+            : false,
     })
 
 
